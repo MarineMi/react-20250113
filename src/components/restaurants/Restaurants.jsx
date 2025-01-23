683679d0f14683d7b4d91a5528b356e9098bbfe2
MarineMi <m.g.vasyukova@gmail.com>
2025-01-23 04:47:24 +0300

    кнопки с названием ресторана

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import { restaurants } from "../constants/mock.js";
 import Restaurant from "../restaurant/Restaurant.jsx";
 
 function Restaurants() {
+  const [activeRestaurant, setActiveRestaurant] = useState(restaurants[0]);
+
   return (
     <>
       <h1>Restaurants</h1>
-      {restaurants.map((rest) => (
-        <Restaurant key={rest.id} restaurant={rest} />
+
+      {restaurants.map((restaurant) => (
+        <button
+          key={restaurant.id}
+          onClick={() => {
+            console.log(restaurant.id);
+            setActiveRestaurant(
+              ...restaurants.filter((r) => r.id == restaurant.id)
+            );
+          }}
+        >
+          {restaurant.name}
+        </button>
       ))}
+
+      <Restaurant key={activeRestaurant.id} restaurant={activeRestaurant} />
     </>
   );
 }
